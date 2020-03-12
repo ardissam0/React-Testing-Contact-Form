633d899c58bbebf9ada37e2f92c1ec3bb4bc95c9
Sam Ardis <ardis.productions@gmail.com>
2020-03-12 14:27:47 -0400

    Added header element in app.js and tested for it in app.test.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ContactForm from "./components/ContactForm";
 export default function App() {
   return (
     <div className="App">
+      <h1>Add Contact Information</h1>
       <ContactForm />
     </div>
   );
